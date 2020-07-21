d1bb5def82fa04bfb95238afd9994f3611d50854
Jose Carlos <josealarconchacon@gmail.com>
2020-07-21 17:34:54 -0400

    create a linked list node

--- linked-list/MyPlayground1.playground/Contents.swift
+++ linked-list/MyPlayground1.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var str = "Hello, playground"
+public class LinkedListNode<T> {
+    var value: T
+    var next: LinkedListNode?
+    var previous: LinkedListNode?
+    
+    public init(value: T) {
+        self.value = value
+    }
+}
+
+public class LinkedList<T> {
+    public typealias Node = LinkedListNode<T>
+    private var head: Node?
+    
+    public var firstElement: Node? {
+        return head
+    }
+    public var lastElement: Node? {
+        guard var current_node = head else {
+            return nil
+        }
+        while let next = head?.next {
+            current_node = next
+        }
+        return current_node
+    }
+}
+
+let list = LinkedList<String>()
+list.firstElement
