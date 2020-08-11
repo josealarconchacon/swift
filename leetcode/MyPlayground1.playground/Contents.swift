ae76ca89b76c2150f2e9c3d8a4f1b3d91b5483f8
Jose Carlos <josealarconchacon@gmail.com>
2020-08-11 13:21:56 -0400

    Two Sum

---
+++ leetcode/MyPlayground1.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Two Sum
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var dic = [Int: Int]()
+    for(num1, num2) in nums.enumerated() {
+        let value = target - num2
+        if let result = dic[value] {
+            return [result, num1]
+        }
+        dic[num2] = num1
+    }
+    return [0,2]
+}
+let array = [1,3,5,7,10]
+print(twoSum(array, 11))
