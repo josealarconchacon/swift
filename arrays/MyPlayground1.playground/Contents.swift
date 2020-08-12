73b5b08def24f674db608017553c4a0b3bbf7c28
Jose Carlos <josealarconchacon@gmail.com>
2020-08-12 13:34:54 -0400

    check if the first element and the last element of the array of integers are equal

--- arrays/MyPlayground1.playground/Contents.swift
+++ arrays/MyPlayground1.playground/Contents.swift
@@ -12,7 +12,7 @@ let first = numArray.first
 //append elements in array
 numArray.append(77)
 
-// 1. Write a Swift program to check if 5 appears as either the first or last element in a given array of integers. The array length should be 1 or more.
+// Write a Swift program to check if 5 appears as either the first or last element in a given array of integers. The array length should be 1 or more.
 func check_5(_ array: [Int]) -> Bool {
     if array.first == 5 || array.last == 5 {
         return true
@@ -21,3 +21,15 @@ func check_5(_ array: [Int]) -> Bool {
     }
 }
 print(check_5([1, 2, 5])) // true
+
+
+// Write a Swift program to check whether the first element and the last element of a given array of integers are equal. The array length must be 1 or more
+func is_equal(_ arr: [Int]) -> Bool {
+    if arr.first == arr.last {
+        return true
+    } else {
+        return false
+    }
+}
+print(is_equal([1, 2, 3]))
+print(is_equal([1, 2, 3, 1]))
