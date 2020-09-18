c703fb5325393b8fbf05966dbccb655740baaea9
Jose Carlos <josealarconchacon@gmail.com>
2020-09-18 11:34:04 -0400

    string

--- swift_basics/types_operators.playground/Contents.swift
+++ swift_basics/types_operators.playground/Contents.swift
@@ -27,3 +27,16 @@ if hungry && !vegetarian {
 hungry ? print("Is time to eat") : print("Let's wait 5 more minutes")
 hungry || thereIsPie ? print("Let's eat that Pie") : print("there is not pie")
 
+// .character property can be access an array of character
+var myStr = "Me me in ST. Louis"
+for char in myStr {
+    if char == "e" {
+        print("found an \(char)")
+    } else {
+        print("not char found")
+    }
+}
+
+// string can be treaed as NSString
+var newStr = myStr.replacingOccurrences(of: "e", with: "5")
+print(newStr)
