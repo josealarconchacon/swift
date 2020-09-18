81dcde703b9da0ba6c1e5b65ab479c608be90c14
Jose Carlos <josealarconchacon@gmail.com>
2020-09-18 11:18:18 -0400

    logical operators

--- swift_basics/types_operators.playground/Contents.swift
+++ swift_basics/types_operators.playground/Contents.swift
@@ -10,4 +10,13 @@ if allowance >= price + jucePrice {
     print("Let just watch a moview at home")
 }
 
-
+// logical operators
+var hungry = true
+var vegetarian = true
+if hungry && !vegetarian {
+    print("Let's eat steak!")
+} else if hungry && vegetarian {
+    print("How about pumpkin curry?")
+} else {
+    print("nevermind")
+}
