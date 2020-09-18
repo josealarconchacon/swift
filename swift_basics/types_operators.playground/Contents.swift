6a8b5390ec88f60472d90255559f230b5133ced8
Jose Carlos <josealarconchacon@gmail.com>
2020-09-18 11:13:49 -0400

    comparison operators

---
+++ swift_basics/types_operators.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// comparison operators
+var price = 7.5
+var allowance = 10.0
+var jucePrice = 2.5
+if allowance >= price + jucePrice {
+    print("Let's go to the movie theater")
+} else {
+    print("Let just watch a moview at home")
+}
+
+
