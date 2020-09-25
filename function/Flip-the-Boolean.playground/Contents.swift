49e2d20c379816292d985c7c8019dda85d37e01b
Jose Carlos <josealarconchacon@gmail.com>
2020-09-25 16:43:35 -0400

    Flip the Boolean

---
+++ function/Flip-the-Boolean.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+/*
+ Create a function that reverses a boolean value.
+ */
+func reverse_boolean(_ flip: Bool) -> Bool {
+    if !flip {
+        return true
+    } else {
+        return false
+    }
+}
