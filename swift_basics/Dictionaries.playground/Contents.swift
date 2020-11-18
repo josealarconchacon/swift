c01679fd819f2e62c75f3e0cac7c4760fc2ab158
Jose Carlos <josealarconchacon@gmail.com>
2020-11-18 14:27:59 -0500

    Modifying a Dictionary

--- swift_basics/Dictionaries.playground/Contents.swift
+++ swift_basics/Dictionaries.playground/Contents.swift
@@ -9,3 +9,13 @@ dict = [:]
 var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
 // Accessing and Modifying a Dictionary
 print("The airports dictionary contains \(airports.count) items.")
+
+if airports.isEmpty {
+    print("The airports dictionary is empty.")
+} else {
+    print("The airports dictionary is not empty.")
+}
+// add a new item to a dictionary
+airports["JFK"] = "Kennedy"
+//change the value associated with a particular key
+airports["JFK"] = "John F. Kennedy"
