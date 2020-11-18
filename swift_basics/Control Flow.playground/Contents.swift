df7920100e642f6b099115b8d986632def92cbe4
Jose Carlos <josealarconchacon@gmail.com>
2020-11-18 14:42:35 -0500

    access its key-value pairs in a Dictionary

---
+++ swift_basics/Control Flow.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// access its key-value pairs in a Dictionary
+
+let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+for (animalName, legCount) in numberOfLegs {
+    print("\(animalName)s have \(legCount) legs")
+}
+
