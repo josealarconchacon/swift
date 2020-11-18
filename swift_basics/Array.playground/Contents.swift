6644a019f64eb8ec5d769a9517d152eb07f17a9f
Jose Carlos <josealarconchacon@gmail.com>
2020-11-18 13:08:25 -0500

    Adding Two Arrays Together

--- swift_basics/Array.playground/Contents.swift
+++ swift_basics/Array.playground/Contents.swift
@@ -10,3 +10,10 @@ item.append(2)
 // Array with a Default Value
 var twoItem = Array(repeating: 0.0, count:2) // twoItem is of type [Double], and equals [0.0, 0.0]
 print(twoItem)
+
+
+// Adding Two Arrays Together
+var anotherTwoItem = Array(repeating: 2.5, count: 2) // [2.5, 2.5]
+var fourItem = twoItem + anotherTwoItem
+print(fourItem)
+
