cbdf60f55b71e6b02684e6657b50aaebb0ac7596
Jose Carlos <josealarconchacon@gmail.com>
2020-11-18 14:36:58 -0500

    Iterating Over a Dictionary

--- swift_basics/Dictionaries.playground/Contents.swift
+++ swift_basics/Dictionaries.playground/Contents.swift
@@ -19,3 +19,34 @@ if airports.isEmpty {
 airports["JFK"] = "Kennedy"
 //change the value associated with a particular key
 airports["JFK"] = "John F. Kennedy"
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue).")
+}
+
+// remove a key-value pair from a dictionary by assigning a value of nil for that key:
+airports["APL"] = "Apple International"
+print(airports)
+airports["APL"] = nil
+print(airports)
+
+// remove a key-value pair from a dictionary with the removeValue(forKey:)
+if let removedValue = airports.removeValue(forKey: "DUB") {
+    print("The removed airport's name is \(removedValue).")
+} else {
+    print("The airports dictionary does not contain a value for DUB.")
+}
+
+// Iterating Over a Dictionary
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
+
+for airportCode in airports.keys {
+    print("Airport code: \(airportCode)")
+}
+
+for airportName in airports.values {
+    print("Airport name: \(airportName)")
+}
+
