89f4bac9c29d540da9e8b686899d776e743fbf5a
Jose Carlos <josealarconchacon@gmail.com>
2020-11-19 11:53:21 -0500

    Fizz Buzz

--- swift_basics/Control Flow.playground/Contents.swift
+++ swift_basics/Control Flow.playground/Contents.swift
@@ -7,3 +7,18 @@ for (animalName, legCount) in numberOfLegs {
     print("\(animalName)s have \(legCount) legs")
 }
 
+
+// Fizz Buzz
+for num in 0...100 {
+    if num % 3 == 0 {
+        if num % 5 == 0 {
+            print("Fizz Buzz")
+        } else {
+            print("Fizz Buzz")
+        }
+    } else if num % 5 == 0 {
+        print("Buzz")
+    } else {
+        print(num)
+    }
+}
