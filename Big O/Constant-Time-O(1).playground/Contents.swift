b97afc2557ab3ba785e4dbfa29728e6c4285b315
Jose Carlos <josealarconchacon@gmail.com>
2020-11-24 12:28:44 -0500

    Hashmap Lookup

--- Big O/Constant-Time-O(1).playground/Contents.swift
+++ Big O/Constant-Time-O(1).playground/Contents.swift
@@ -46,3 +46,37 @@ execTime = BenchTime.measureBlock(closure: {
 print("Averags execution time: \(execTime)")
 
 
+
+/*
+ Hashmap Lookup
+ The Hashmap Lookup also run in constant time O(1)
+ */
+
+let dictionary = ["one": 1, "two": 2, "three": 3]
+execTime = BenchTime.measureBlock(closure: {
+    _ = dictionary["two"]
+})
+print("The average execution for the dictionary: \(execTime)")
+
+// func take an input argument call size,
+// the argument provide the size of the generateDict dictionary,
+// the function return Dictionary with key of type String, and value of time Integer
+func generateDict(size: Int) -> Dictionary<String, Int> {
+    // empty Dictionary
+    var result = Dictionary<String, Int>()
+    // if the size is no bigger than zero, return the empty Dictionary
+    guard size > 0 else {
+        return result
+    }
+    for i in 0..<size {
+        let key = String()
+        result[key] = i
+    }
+    return result
+}
+
+var otherDict = generateDict(size: 500)
+execTime = BenchTime.measureBlock(closure: {
+    _ = otherDict["324"]
+})
+print("The average execution for the dictionary: \(execTime)")
