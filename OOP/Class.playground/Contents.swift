63b2ede0ae7396922cb01d65f9dd51ea67255e47
Jose Carlos <josealarconchacon@gmail.com>
2020-12-02 14:47:55 -0500

    Create Object from class

--- OOP/Class.playground/Contents.swift
+++ OOP/Class.playground/Contents.swift
@@ -12,9 +12,31 @@ class House{
         print("Opening door")
     }
     
+    func describe() {
+        print("Your house is locate at \(address)")
+    }
+    
+    init() {}
     // Initializers
     init(address: String) {
         self.address = address
     }
 }
 
+
+// Create Objects from the class
+let myHouse = House()
+
+// Access data via dot notation
+myHouse.address = "435 Dekald St"
+myHouse.address
+
+// Invoke methods via dot notation
+myHouse.openDoor()
+
+
+let seconHouse = House(address: "435 Main St")
+seconHouse.address
+seconHouse.numberOfFloors = 9
+
+seconHouse.describe()
