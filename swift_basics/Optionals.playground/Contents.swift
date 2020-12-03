64e6ebc50fc16ac671809eae7cc0c44b67e19413
Jose Carlos <josealarconchacon@gmail.com>
2020-12-02 20:33:17 -0500

    Optional binding

--- swift_basics/Optionals.playground/Contents.swift
+++ swift_basics/Optionals.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var str = "Hello, playground"
+// Optional
+
+
+var name: Optional<String> = "Jhon"
+print("Hello \(name)")
+
+
+// Unconditional unwrapping
+//print("Hello \(name!)")
+
+
+// Optional binding
+if let myName = name {
+    print("Hello \(myName)")
+}
+
+// Nil coalescing operator
+print("Hello \(name ?? "Tomy")")
