2a7716c66f088c02647e052f525ab159643b900e
Jose Carlos <josealarconchacon@gmail.com>
2020-12-04 12:41:20 -0500

    two sum

--- leetcode/TwoSum.playground/Contents.swift
+++ leetcode/TwoSum.playground/Contents.swift
@@ -2,21 +2,20 @@ import UIKit
 
 
 func two_sum(_ n: [Int], target: Int) -> [Int] {
-    var dict = [Int: Int]()
-    let num = n.sorted()
-    for (i,j) in num.enumerated() {
-        let value = target - j
-        if let selectNum = dict[value] {
-            return [selectNum, i]
-        }
-        dict[j] = i
+    var dictionary = [Int: Int]()
+    for(i,y) in n.enumerated() {
+        let value = target - y
+        if let addNum = dictionary[value] {
+            return [addNum, i]
+            }
+        dictionary[y] = i
     }
-    return [0,1]
+  return [0,1]
 }
 
 
 let array = [2,7,11,15]
 print(two_sum(array, target: 17))  // [0, 3]
 
-let array1 = [3,3]
-print(two_sum(array1, target: 6)) // [0, 1]
+let array1 = [3,2,4]
+print(two_sum(array1, target: 6)) // [1, 2]
