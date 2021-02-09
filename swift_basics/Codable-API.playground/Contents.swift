766046612dd25926a0afb5fa675cc9fdfd94cf6c
Jose Carlos <josealarconchacon@gmail.com>
2021-02-09 18:36:47 -0500

    Encode an object into a JSON string

--- swift_basics/Codable-API.playground/Contents.swift
+++ swift_basics/Codable-API.playground/Contents.swift
@@ -13,4 +13,13 @@ class Person: Codable {
     }
 }
 
-
+// Objects
+do {
+    let amy = Person(name: "Amy", age: 99, loveApple: true)
+    let encoder = JSONEncoder()
+    let data = try encoder.encode(amy)
+    let jsonString = String(data: data, encoding: .utf8)!
+    print("json strin: \(jsonString)")
+} catch {
+    print("error encoding person: \(error.localizedDescription)")
+}
