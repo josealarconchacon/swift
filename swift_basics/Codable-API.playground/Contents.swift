8fea3cf893ba452e1ac493aaf49ecf2785be7518
Jose Carlos <josealarconchacon@gmail.com>
2021-02-09 18:47:50 -0500

    Decode string into object

--- swift_basics/Codable-API.playground/Contents.swift
+++ swift_basics/Codable-API.playground/Contents.swift
@@ -13,7 +13,8 @@ class Person: Codable {
     }
 }
 
-// Objects
+// Encode
+// encode a swift object into a JSON string
 do {
     let amy = Person(name: "Amy", age: 99, loveApple: true)
     let encoder = JSONEncoder()
@@ -22,4 +23,22 @@ do {
     print("json strin: \(jsonString)")
 } catch {
     print("error encoding person: \(error.localizedDescription)")
+}
+
+
+
+// Decode
+// convert a JSON string into a swift object
+do {
+    let jsonString = "{\"name\": \"Pedero\", \"age\": 109, \"loveApple\": false} "
+    let decoder = JSONDecoder()
+    let data = jsonString.data(using: .utf8)!
+    
+    // convert string into an object
+    let pedro = try decoder.decode(Person.self, from: data)
+    pedro.name
+    pedro.loveApple
+    pedro.age
+} catch {
+    print("error decoding person: \(error.localizedDescription)")
 }
