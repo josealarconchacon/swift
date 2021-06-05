dc9af21587f992f31723ab3c1a8e0523b85a97dd
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 21:12:19 -0400

    factorial of a number

--- Recursion/Recursion.playground/Contents.swift
+++ Recursion/Recursion.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
+// A function that calls itself is known as a recursive function
 
-
+func factorial(num: Int) -> Int {
+    if(num == 0) {
+        return 1
+    }
+    return num * factorial(num: num - 1)
+}
+print(factorial(num: 4))
