0bf76007b6a63971cb5134af714298f2ec0be2bd
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 21:18:10 -0400

    Implement Power Function Using Recursion

--- Recursion/Recursion.playground/Contents.swift
+++ Recursion/Recursion.playground/Contents.swift
@@ -7,4 +7,19 @@ func factorial(num: Int) -> Int {
     }
     return num * factorial(num: num - 1)
 }
+
 print(factorial(num: 4))
+print("---------------------------------------------------------------")
+
+
+// Implement Power Function Using Recursion
+func power(num: Int, pow: Int) -> Int {
+    if(pow == 0) {
+        return 1
+    } else {
+        return num * power(num: num, pow: pow - 1)
+    }
+}
+
+let the_power = power(num: 2, pow: 3)
+print(the_power)
