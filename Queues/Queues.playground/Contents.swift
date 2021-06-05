42145d616f2b3af3e2338c6cc7dfea4c656e72d7
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 21:06:57 -0400

    enqueue and dequeue element

--- Queues/Queues.playground/Contents.swift
+++ Queues/Queues.playground/Contents.swift
@@ -12,4 +12,33 @@ struct Queue<Element> {
         return array.first
     }
     
+    mutating func enqueue(_ element: Element) -> Bool { // add item to queue
+        array.append(element)
+        return true
+    }
+    
+    mutating func dequeue() -> Element? { // remove item from queue
+        if(isEmpty) {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
 }
+
+extension Queue: CustomDebugStringConvertible {
+    var debugDescription: String {
+        return String(describing: array)
+    }
+}
+ var queue = Queue<Int>()
+queue.enqueue(20)
+queue.enqueue(40)
+queue.enqueue(60)
+queue.enqueue(80)
+print(queue)
+
+queue.dequeue()
+print(queue)
+
+
